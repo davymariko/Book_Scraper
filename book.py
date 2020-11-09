969b2064a52e866184bb97a00b829915d0c97c25
Davy Nimbona <davynimbona@gmail.com>
2020-11-09 19:04:52 +0100

    Plus de fonctions à utiliser pour la classe BookScraper

--- book.py
+++ book.py
@@ -3,70 +3,63 @@ from bs4 import BeautifulSoup
 import lxml
 import re
 
-def get_description(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    price = soup.find_all('p')
+def get_description(delicious_soup):
+    price = delicious_soup.find_all('p')
     check = 0
     print("Description: \n" , price[3].text)
     
 
-def get_title(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    title = soup.find('h1')
+def get_title(delicious_soup):
+    title = delicious_soup.find('h1')
     print("TITLE: " , title.text)
 
 
-def get_upc(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    upc = soup.find_all('td')
+def get_upc(delicious_soup):
+    upc = delicious_soup.find_all('td')
     print("UPC : " , upc[0].text)
 
 
-def get_price_tax(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    price_tax = soup.find_all('td')
+def get_price_tax(delicious_soup):
+    price_tax = delicious_soup.find_all('td')
     print("Prix avec taxe inclus : " , price_tax[2].text)
 
 
-def get_price_tax_exclu(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    price_tax_exclu = soup.find_all('td')
+def get_price_tax_exclu(delicious_soup):
+    price_tax_exclu = delicious_soup.find_all('td')
     print("Prix avec taxe exclus : " , price_tax_exclu[3].text)
 
 
-def get_number_available(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    number_available = soup.find_all('td')
+def get_number_available(delicious_soup):
+    number_available = delicious_soup.find_all('td')
     print("Available in stock : " , re.findall(r'\d+', number_available[5].text)[0])
 
 
-def get_category(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    category = soup.find_all('li')
+def get_category(delicious_soup):
+    category = delicious_soup.find_all('li')
     print(category[2].text.strip())
 
-def get_image_url(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    image = soup.find_all('div', {'class':'item active'})
-    print(image[1]['src'])
+def get_image_url(delicious_soup):
+    image = delicious_soup.find_all('img')
+    # this will return src attrib from img tag that is inside 'a' tag
+    link_pattern ="http://books.toscrape.com/"
+    print(link_pattern + str(image[0]['src'][6:]))
+
+
+def get_rating(url):
+    pass
 
 
 if __name__ == "__main__":
     url = "http://books.toscrape.com/catalogue/art-and-fear-observations-on-the-perils-and-rewards-of-artmaking_559/index.html"
-    get_description(url)
-    get_title(url)
-    get_upc(url)
-    get_price_tax(url)
-    get_price_tax_exclu(url)
-    get_number_available(url)
-    get_category(url)
-    get_image_url(url)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    get_description(soup)
+    get_title(soup)
+    get_upc(soup)
+    get_price_tax(soup)
+    get_price_tax_exclu(soup)
+    get_number_available(soup)
+    get_category(soup)
+    get_image_url(soup)
 
