d07941831fa2265eecd769465cacec033f2ef93f
Davy Nimbona <davynimbona@gmail.com>
2020-11-16 01:21:33 +0100

    modifier la fonction de save_csv

--- save_data.py
+++ save_data.py
@@ -1,25 +1,40 @@
 import pandas as pd
 import requests
+import os
 
+PATH = os.getcwd()
+os.chdir(PATH)
 
-def save_data(data_list):
+def save_csv(category_name, books_list):
+    """
+    Fonction qui permet de sauvegarder les livres d'une categorie specifique dans un fichier csv
+
+    :param category_name: Un string qui specifie la categorie de la liste des livres à sauvegarder
+    :param books_list: Une liste de donnees de livres d'une categorie specifique
+    """
+
+    location = "Assets/CSV_Files/" + category_name.replace(' ', '_')
+
+    os.makedirs(location)
 
     # Create the pandas DataFrame
-    df = pd.DataFrame(data_list, columns = ['Product Page Url', 'UPC', 'Title', 
+    df = pd.DataFrame(books_list, columns = ['Product Page Url', 'UPC', 'Title', 
         'Price Including Tax', 'Price Excluding Tax', 'Number Available',
         'Product Description', 'Category', 'Rating', 'Image Url'])
     
     # Save dataframe
-    df.to_csv('Assets/File.csv') 
+    df.to_csv(location +'/File.csv')
 
 
-def save_image(image_url, name):
+def save_image(image_url):
+    """
+    Fonction qui permet de sauvegarder l'image du livre visite
+
+    :param image_url: Le lien de l'image à sauvegarder
+    """
     response = requests.get(image_url)
-    location = "Assets/Images/" + name + '.png'
-    
+    location = "Assets/Images/"
+
     file = open(location, "wb")
     file.write(response.content)
     file.close()
-
-if __name__ == "__main__":
-    save_image("http://books.toscrape.com/media/cache/58/bb/58bb81b052b6ad90adcd57e95da99a50.jpg", "This book")
