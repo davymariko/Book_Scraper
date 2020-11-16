9d246cfb3eca0e60ace615239fad023a05bd7e2f
Davy Nimbona <davynimbona@gmail.com>
2020-11-16 01:11:38 +0100

    re modifier le draft, à effacer

--- book.py
+++ book.py
@@ -28,9 +28,11 @@ def get_price_tax(delicious_soup):
 
 
 def get_price_tax_exclu(delicious_soup):
-    price_tax_exclu = delicious_soup.find_all('td')
+    price_tax_exclu = delicious_soup.find('table').find_all('td')[2]
 
-    return (float(re.findall(r"\d+\.\d+", price_tax_exclu[3].text)[0]))
+    return float(re.findall(r"\d+\.\d+", price_tax_exclu.text)[0])
+
+    # return (float(re.findall(r"\d+\.\d+", price_tax_exclu[3].text)[0]))
     
 
 def get_number_available(delicious_soup):
@@ -40,9 +42,11 @@ def get_number_available(delicious_soup):
 
 
 def get_category(delicious_soup):
-    category = delicious_soup.find_all('li')
+    category = delicious_soup.find('li , {'class' : 'active'}')
 
-    return (category[2].text.strip())
+    return category
+
+    # return (category[2].text.strip())
 
 def get_image_url(delicious_soup):
     image = delicious_soup.find_all('img')
@@ -53,21 +57,34 @@ def get_image_url(delicious_soup):
 
 
 def get_rating(delicious_soup):
-    # ratings = delicious_soup.find_all()
-    pass
+    ratings = delicious_soup.find_all('p', {'class': 'star-rating'})
+
+    if ratings[0]['class'][1] == 'One':
+        rating = 1
+    elif ratings[0]['class'][1] == 'Two':
+        rating = 2
+    elif ratings[0]['class'][1] == 'Three':
+        rating = 3
+    elif ratings[0]['class'][1] == 'Four':
+        rating = 4
+    else:
+        ratings = 5
+
+    return(rating)
 
 
 if __name__ == "__main__":
-    url = "http://books.toscrape.com/catalogue/art-and-fear-observations-on-the-perils-and-rewards-of-artmaking_559/index.html"
+    url = "http://books.toscrape.com/catalogue/suddenly-in-love-lake-haven-1_835/index.html"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
 
-    print(get_description(soup))
-    print(get_title(soup))
-    print(get_upc(soup))
-    print(get_price_tax(soup))
-    print(get_price_tax_exclu(soup))
-    print(get_number_available(soup))
-    print(get_category(soup))
-    print(get_image_url(soup))
+    # print(get_description(soup))
+    # print(get_title(soup))
+    # print(get_upc(soup))
+    # print(get_price_tax(soup))
+    # print(get_price_tax_exclu(soup))
+    # print(get_number_available(soup))
+    # print(get_category(soup))
+    # print(get_image_url(soup))
+    # print(get_rating(soup))
 
