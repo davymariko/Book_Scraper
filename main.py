347a0da857e27c7d00096c059030b186ab805dba
Davy Nimbona <davynimbona@gmail.com>
2020-11-19 01:51:42 +0100

    Main final (Croisons les doigts)

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
-from category import get_category_urls
-from category import get_href_links
-from category import book_urls_per_category
-from category import books_data_per_category
-from book_scraper import BookScraper
-from save_data import save_csv
-from save_data import save_image
-from threading import Thread
+from src.category import CategoryClass
+from src.category import get_category_urls
+from src.save_data import save_csv
+from src.save_data import save_image
 import time
-    
+import os
 
-if __name__ == "__main__":
-    book_data = []
+def main():
     print("Bienvenue à Bookscraper\n\n")
     start_time = time.time()
 
     categories = get_category_urls("http://books.toscrape.com/")
 
-    check = 1
+    book_data = []
+
     for category_name, category_url in categories:
-        book_links = book_urls_per_category(category_url)
-
-        print(check)
-
-        print("Book Links: --- %s seconds ---" % (time.time() - start_time))
-        
-        book_data = books_data_per_category(book_links)
-
-        print("Book Data: --- %s seconds ---" % (time.time() - start_time))
-
+        this_category = CategoryClass(category_url)
+        book_data = this_category.books_data_per_category()
         save_csv(category_name, book_data)
-
-        print("Save CSV: --- %s seconds ---" % (time.time() - start_time))
-
-        # book_image_urls = [url[9] for url in book_data]
-
-        # print("Image url list: --- %s seconds ---" % (time.time() - start_time))
-
-        # save_image(book_image_urls)
-
-        # print("Save Image: --- %s seconds ---" % (time.time() - start_time))
-
-        check += 1
+        book_image_urls = [url[9] for url in book_data]
+        save_image(book_image_urls)
 
     print("--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == "__main__":
+    main()
