4a33cf2098b4168fd5a3095718b1b0296822b70f
Davy Nimbona <davynimbona@gmail.com>
2020-11-20 18:14:48 +0100

    Revoir si une image n'existe pas déjà dans le local repo

--- scrapper/link_request/page_request.py
+++ scrapper/link_request/page_request.py
@@ -1,9 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import os
-
-# PATH = os.getcwd()
-# os.chdir(PATH)
+import re
 
 
 def page_content(page_url):
@@ -20,23 +18,29 @@ def page_content(page_url):
         print(f'Message: {e}')
 
     return BeautifulSoup(response, 'lxml')
-    
 
-def save_image(image_urls):
+
+def save_image(images_data):
     """
     Fonction qui permet de sauvegarder l'image du livre visite
-
-    :param image_urls: Liste contenant les liens des images 
-    des livres selectionnés 
+    :param images_data: Liste contenant les liens des images et
+    les titre des livres selectionnés qui seront les noms des images
     """
     location = "Assets/Images/"
     if not os.path.exists(location):
         os.makedirs(location)
-        
-    for link in image_urls:
-        file_location = location + link.split("/")[-1]
-        try:
-            urllib.request.urlretrieve(link, file_location)
-        except:
-            print(f'Echec: Telecharger fichier au lien: {link}')
+    
+    for image_url, book_name in images_data:
+        book_name = re.sub(r"[^a-zA-Z0-9]+", ' ', book_name)
+        image_name = book_name.replace(" ", "_") + '.png'
+        file_location = location + image_name
 
+        if os.path.exists(file_location):
+            print(f"\"{file_location}\" already exists")
+            print(f"Image URL: {image_url}")
+
+        try:
+            urllib.request.urlretrieve(image_url, file_location)
+        except Exception as e:
+            print(f'[Echec] Telecharger fichier au lien: {image_url}')
+            print(f"Message: {e}")
